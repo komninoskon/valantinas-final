209cf9a11817537ae24447be713fa6af6b20995c
komninoskon <komninosk99@gmail.com>
2025-05-20 01:50:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def reservation():
     return render_template('reservation.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
